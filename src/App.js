d67b2f8024cac46efaea93935c5c14270907a3b7
hajhouj <alaa.spread@gmail.com>
2026-01-23 22:32:10 +0100

    un peu de pratique

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <>
-       <Cube></Cube>
+      <Cube></Cube>
     </>
 
   );
--- src/Cube.jsx
+++ src/Cube.jsx
@@ -1,14 +1,18 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+
 
 function Cube() {
   const ref1=useRef()
   const ref2=useRef(0)
   const tableau=[useRef(),useRef(),useRef(),useRef(),useRef(),useRef()]
   const ref6=useRef("ferme")
-
+  const ref111=useRef()
+  const refAnimer=useRef(true)
+  const [state,setState]=useState(0)
 
   const tester=()=>{
-   ref2.current+=20
+   ref2.current+=4
    ref1.current.style.transform=`rotateX(${ref2.current}deg) rotateY(${ref2.current}deg)`
   }
 
@@ -46,17 +50,63 @@ function Cube() {
   }
 
 
+  const tester11=()=>{
+     tableau.forEach((ele)=>{
+        ele.current.style.transition="1s all ease"
+        ele.current.style.opacity=0.2
+     })
+  }
+
+
+  useEffect(()=>{
+    window.addEventListener("mousemove",(e)=>{
+      if(refAnimer.current){
+            ref111.current.style.position="fixed"
+            const X=e.clientX-150;
+            const Y=e.clientY-150;
+            ref111.current.style.left=`${X}px`
+            ref111.current.style.top=`${Y}px`
+      }
+    })
+
+    window.addEventListener("click",()=>{
+        refAnimer.current=refAnimer.current?false:true
+        console.log(refAnimer.current)
+    })
+  },[])
+
+
+  const tester222=async()=>{
+    const promise1=new Promise((res,rej)=>{
+        setTimeout(() => {
+            res("hello")
+        }, 3000);
+    })
+    
+    setTimeout(() => {
+        console.log("tester tester tester mee")
+    }, 1000);
+    const a=await promise1
+    console.log(a)
+  }
+
+
+  useEffect(()=>{
+    console.log("executée")
+  },)
+
+
   return (
-    <div style={{width:"300px",height:"300px",display:"flex",gap:"100px",flexDirection:"column",justifyContent:"center",alignItems:"center",perspective:"1000px"}}>
-       <div onMouseMove={tester} ref={ref1} style={{width:"200px",flexShrink:0,height:"200px",position:"relative",transformStyle:"preserve-3d",transition:"all",transitionDuration:"2s"}}>
-          <div ref={tableau[0]} style={{background:"red",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(0deg) translateZ(100px)"}}>0</div>
-          <div ref={tableau[1]} style={{background:"green",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(180deg) translateZ(100px)"}}>180</div>
-          <div ref={tableau[2]} style={{background:"yellow",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(90deg) translateZ(100px)"}}>90</div>
-          <div ref={tableau[3]} style={{background:"purple",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(270deg) translateZ(100px)"}}>270</div>
-          <div ref={tableau[4]} style={{background:"brown",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateY(90deg) translateZ(100px)"}}>90Y</div>
-          <div ref={tableau[5]} style={{background:"pink",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateY(-90deg) translateZ(100px)"}}>-90Y</div>
+    <div onClick={tester222} ref={ref111} style={{width:"300px",height:"300px",display:"flex",gap:"100px",flexDirection:"column",justifyContent:"center",alignItems:"center",perspective:"1000px"}}>
+       <div onMouseMove={tester}  ref={ref1} style={{width:"200px",flexShrink:0,height:"200px",position:"relative",transformStyle:"preserve-3d",transition:"all",transitionDuration:"2s"}}>
+          <div ref={tableau[0]} style={{background:"red",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(0deg) translateZ(100px)",color:"white",fontSize:"30px",display:"flex",justifyContent:"center",alignItems:"center"}}>0</div>
+          <div ref={tableau[1]} style={{background:"green",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(180deg) translateZ(100px)",color:"white",fontSize:"30px",display:"flex",justifyContent:"center",alignItems:"center"}}>180</div>
+          <div ref={tableau[2]} style={{background:"yellow",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(90deg) translateZ(100px)",color:"white",fontSize:"30px",display:"flex",justifyContent:"center",alignItems:"center"}}>90</div>
+          <div ref={tableau[3]} style={{background:"purple",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateX(270deg) translateZ(100px)",color:"white",fontSize:"30px",display:"flex",justifyContent:"center",alignItems:"center"}}>270</div>
+          <div ref={tableau[4]} style={{background:"brown",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateY(90deg) translateZ(100px)",color:"white",fontSize:"30px",display:"flex",justifyContent:"center",alignItems:"center"}}>90Y</div>
+          <div ref={tableau[5]} style={{background:"pink",opacity:0.6,width:"100%",height:"100%",position:"absolute",top:0,right:0,transform:"rotateY(-90deg) translateZ(100px)",color:"white",fontSize:"30px",display:"flex",justifyContent:"center",alignItems:"center"}}>-90Y</div>
        </div>
-       <button onClick={tester1} className='bg-blue-500 text-white p-5 rounded-[12px] hover:bg-blue-300 cursor-pointer'>click</button>
+       <button onClick={tester1} className='bg-blue-500 text-white p-5 mt-10 rounded-[12px] hover:bg-blue-300 cursor-pointer'>click</button>
     </div>
   )
 }
